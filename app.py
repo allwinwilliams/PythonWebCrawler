0a6103f3227d2c66496e0dd32e8bcd4211a2ddb3
Allwin Williams <allwinwilliams.info@gmail.com>
2017-09-17 20:42:04 +0530

    most bascis

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
-from flask import Flask, request, jsonify
-
 import os
-
-from parser import *
-from spider import getPageList
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -14,25 +10,6 @@ def hello():
 @app.route('/test',methods=['GET'])
 def test_func():
     return jsonify("Hello, World!")
-
-# @app.route('/crawler/api/v1.0/get-page', methods=['GET'])
-# def pageParse():
-#     page_url = request.args.get('url')
-#     if page_url:
-#         page = getPage(page_url)
-#         print(str(page))
-#         return jsonify(page)
-#     return jsonify("no url sent")
-#
-#
-# @app.route('/crawler/api/v1.0/get-list', methods=['GET'])
-# def pageList():
-#     page_url = request.args.get('url')
-#     if page_url:
-#         pages = getPageList(page_url)
-#         print(str(pages))
-#         return jsonify(pages)
-#     return jsonify("no url attribute")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
