12cd6260580fc79f1b1258191e80cde3866ca09a
Allwin Williams <allwinwilliams.info@gmail.com>
2017-09-17 20:38:03 +0530

    basics

--- app.py
+++ app.py
@@ -15,24 +15,24 @@ def hello():
 def test_func():
     return jsonify("Hello, World!")
 
-@app.route('/crawler/api/v1.0/get-page', methods=['GET'])
-def pageParse():
-    page_url = request.args.get('url')
-    if page_url:
-        page = getPage(page_url)
-        print(str(page))
-        return jsonify(page)
-    return jsonify("no url sent")
-
-
-@app.route('/crawler/api/v1.0/get-list', methods=['GET'])
-def pageList():
-    page_url = request.args.get('url')
-    if page_url:
-        pages = getPageList(page_url)
-        print(str(pages))
-        return jsonify(pages)
-    return jsonify("no url attribute")
+# @app.route('/crawler/api/v1.0/get-page', methods=['GET'])
+# def pageParse():
+#     page_url = request.args.get('url')
+#     if page_url:
+#         page = getPage(page_url)
+#         print(str(page))
+#         return jsonify(page)
+#     return jsonify("no url sent")
+#
+#
+# @app.route('/crawler/api/v1.0/get-list', methods=['GET'])
+# def pageList():
+#     page_url = request.args.get('url')
+#     if page_url:
+#         pages = getPageList(page_url)
+#         print(str(pages))
+#         return jsonify(pages)
+#     return jsonify("no url attribute")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
